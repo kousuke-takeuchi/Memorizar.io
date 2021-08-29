c815dd16cb147605da2a0fc763c3b8f75e6432fc
Kousuke Takeuchi <ktakeuchi@keisuu.co.jp>
2021-08-29 23:48:08 +0900

    add newrelic

--- memorizar/wsgi.py
+++ memorizar/wsgi.py
@@ -4,13 +4,19 @@ WSGI config for memorizar project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
+import newrelic.agent
+
 from django.core.wsgi import get_wsgi_application
+
+
+newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), 'newrelic.ini'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memorizar.settings')
 
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
