c80d9ae82f8054616e70789b1f357edc86f9ba5c
Kousuke Takeuchi <ktakeuchi@keisuu.co.jp>
2021-08-31 17:22:47 +0900

    whitenoiseの設定変更

--- memorizar/settings.py
+++ memorizar/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'memorizar.storage.WhiteNoiseStaticFilesStorage'
 
 # Media files
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
